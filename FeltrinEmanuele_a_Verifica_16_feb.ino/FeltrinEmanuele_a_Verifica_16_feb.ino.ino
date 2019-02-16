aadda20b05c70c72285a9bd3198fb53574942402
Feltrin Emanuele <Emanuelefeltrin19971010@gmail.com>
2019-02-16 08:31:35 +0100

    Primo setup

--- FeltrinEmanuele_a_Verifica_16_feb.ino/FeltrinEmanuele_a_Verifica_16_feb.ino.ino
+++ FeltrinEmanuele_a_Verifica_16_feb.ino/FeltrinEmanuele_a_Verifica_16_feb.ino.ino
@@ -1,10 +1,28 @@
+int rosso;
+int giallo;
+int bianco;
+int verde;
+//variabile globare DURATA
+int DURATA;
+
 void setup() {
-  // put your setup code here, to run once:
+  rosso = 11;
+  giallo = 10;
+  bianco = 9;
+  verde = 8;
+
+  pinMode (rosso,OUTPUT);
+  pinMode (giallo,OUTPUT);
+  pinMode (bianco,OUTPUT);
+  pinMode (verde,OUTPUT);
+
+  Serial.begin(9600);
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-int a;
+
 }
